9fa9718ae93630a7252ccc924b9a263ae5312608
Jay Cabasag <jaycabasag1999@gmail.com>
2023-12-17 21:52:33 +0800

    first commit

---
+++ src/main.rs
@@ -0,0 +1,54 @@
+use axum::routing::get;
+use serde_json::Value;
+use socketioxide::{
+    extract::{AckSender, Bin, Data, SocketRef},
+    SocketIo,
+};
+use tracing::info;
+use tracing_subscriber::FmtSubscriber;
+use tower_http::cors::CorsLayer;
+use tower::ServiceBuilder;
+
+fn on_connect(socket: SocketRef, Data(data): Data<Value>) {
+    info!("Socket.IO connected: {:?} {:?}", socket.ns(), socket.id);
+    socket.emit("auth", data).ok();
+
+    socket.on(
+        "message",
+        |socket: SocketRef, Data::<Value>(data), Bin(bin)| {
+            info!("Received event: {:?} {:?}", data, bin);
+            socket.bin(bin).emit("message-back", data).ok();
+        },
+    );
+
+    socket.on(
+        "message-with-ack",
+        |Data::<Value>(data), ack: AckSender, Bin(bin)| {
+            info!("Received event: {:?} {:?}", data, bin);
+            ack.bin(bin).send(data).ok();
+        },
+    );
+}
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    tracing::subscriber::set_global_default(FmtSubscriber::default())?;
+
+    let (layer, io) = SocketIo::new_layer();
+    io.ns("/blockchain", on_connect);
+
+    let app = axum::Router::new()
+        .route("/", get(|| async { "Hello, World!" }))
+        .layer(
+            ServiceBuilder::new()
+                .layer(CorsLayer::permissive()) // Enable CORS policy
+                .layer(layer),
+        );
+
+    info!("Starting server");
+
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
+    axum::serve(listener, app).await.unwrap();
+
+    Ok(())
+}
